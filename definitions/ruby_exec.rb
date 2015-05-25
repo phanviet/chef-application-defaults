fd41e402b9727196f95304b613d1ca2ac9547a7e
Viet Phan <vietphxfce@gmail.com>
2015-05-25 12:06:35 +0700

    update ruby_exec resource

--- definitions/ruby_exec.rb
+++ definitions/ruby_exec.rb
@@ -1,7 +1,7 @@
 define :ruby_exec do
   rvm_shell params[:name] do
     ruby_string "#{node[:app][:ruby_ver]}@#{node[:app][:ruby_gemset]}"
-    cwd params[:release_path]
+    cwd params[:dir]
     user node[:app][:user]
     group node[:app][:group]
 
